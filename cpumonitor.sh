027b97d16602712a870c4fbae44406e0413a6834
spmvg <13852721+spmvg@users.noreply.github.com>
2021-11-09 17:42:49 +0100

    send a warning during excessive memory usage

--- cpumonitor.sh
+++ cpumonitor.sh
@@ -1,4 +1,5 @@
 CPU_WARNING_THRESHOLD=0.8
+MEMORY_AVAILABLE_WARNING_THRESHOLD_KB=1000000
 SLACK_WEBHOOK_URL=secret
 LOG_FOLDER=/var/log/cpumonitor
 DELETION_DAYS=30
@@ -7,6 +8,12 @@ cpu_usage=$(awk '/^cpu / {for(ncol=2; ncol<=NF; ncol++) total+=$ncol; print 1-$5
 cpu_usage_descr=$(echo $cpu_usage | awk '{if ($1 < '$CPU_WARNING_THRESHOLD') print "LOW"; else print "HIGH"}')
 if [ $cpu_usage_descr != "LOW" ]; then
 	curl -X POST -H 'Content-type: application/json' --data '{"text":"CPU WARNING: CPU usage is '$cpu_usage_descr' with a value of '$cpu_usage' out of 1."}' $SLACK_WEBHOOK_URL
+fi
+
+memory_available_kb=$(awk '/^MemAvailable:/ {print $2}' /proc/meminfo)
+memory_available_kb_descr=$(echo $memory_available_kb | awk '{if ($1 < '$MEMORY_AVAILABLE_WARNING_THRESHOLD_KB') print "LOW"; else print "HIGH"}')
+if [ $memory_available_kb_descr != "HIGH" ]; then
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"MEMORY WARNING: available memory is '$memory_available_kb_descr' with a value of '$memory_available_kb' kB."}' $SLACK_WEBHOOK_URL
 fi
 
 new_filename=$(date --iso-8601='seconds').log
